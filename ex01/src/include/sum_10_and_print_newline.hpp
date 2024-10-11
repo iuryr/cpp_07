eb57d3bea977f501abe8ca275e1be032623781eb
iuryr <iuryroberto@gmail.com>
2024-10-11 16:11:17 -0300

    ex01 main with instantiation of function

---
+++ ex01/src/include/sum_10_and_print_newline.hpp
@@ -0,0 +1,13 @@
+#ifndef _SUM10_HPP_
+#define _SUM10_HPP_
+
+#include "qolMacros.hpp"
+
+template<typename T>
+void sum_10_and_print_newline(T x)
+{
+	x+= 10;
+	println(x);
+}
+
+#endif //_SUM10_HPP_
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -1,29 +1,18 @@
 #include "iter.hpp"
+#include "sum_10_and_print_newline.hpp"
 #include "qolMacros.hpp"
-
-void print_int_and_newline(int x)
-{
-	println(x);
-}
-
-int sum_10_and_print_with_newline(float x)
-{
-	x += 10;
-	println(x);
-	return 10;
-}
 
 int main(void)
 {
 	println("Tests with array of ints");
 	int array_of_ints[] = {10, 20, 30, 40, 50};
 
-	iter(array_of_ints, 5, print_int_and_newline);
+	iter(array_of_ints, 5, sum_10_and_print_newline<int>);
 
 	println("");
 	println("Tests with array of floats");
 	float array_of_floats[] = {10.f, 5.f, 10.5f, 8.9f};
 
-	iter(array_of_floats, 4, sum_10_and_print_with_newline);
+	iter(array_of_floats, 4, sum_10_and_print_newline<float>);
 
 }
